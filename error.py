6700690cd329cbf92896e8f4b14daccc3931dd6c
erlendak <erlendak@mail.uio.no>
2019-09-29 19:37:50 +0200

    included more results in our report, and refrences

--- error.py
+++ error.py
@@ -1,10 +1,8 @@
 import matplotlib.pyplot as plt
-import numpy as np 
+import numpy as np
 
-eigen_val1 = []
-eigen_val2 = []
-eigen_val3 = []
-eigen_val4 = []
+tmp = []
+
 
 
 import sys
@@ -14,51 +12,17 @@ with open("2b.dat", 'r') as infile:
     print("Reading from "+"2b.dat")
     for i in infile:
         data = i.split()
-        e_val1= data[0].split()[0]
+        tmp.append(float(data[0]))
 
-        """e_val2= data[1].split("[]")[0]
-        e_val3= data[2].split("[]")[0]
-        e_val4= data[3].split("[]")[0]
-        eigen_val1.append(float(e_val1))
-        eigen_val2.append(float(e_val2))
-        eigen_val3.append(float(e_val3))
-        eigen_val4.append(float(e_val4))"""
-        eigen_val1.append(float(e_val1))
-        #num_val.append(float(n_val))
-        #x_val.append(float(x_v))
 
- print(e_val1)
-"""   
-with open("2d.dat", 'r') as infile:
-    print("Reading from "+"2d.dat")
-    for i in infile:
-        data = i.split()
-        e_val= data[2].split("[]")[0]
-        eigen_val.append(float(e_val))
-        #num_val.append(float(n_val))
-        #x_val.append(float(x_v))
-       
-with open("2e.dat", 'r') as infile:
-    print("Reading from "+"2e.dat")
-    for i in infile:
-        data = i.split()
-        e_val= data[0].split("[]")[0]
-        eigen_val.append(float(e_val))
-        #num_val.append(float(n_val))
-        #x_val.append(float(x_v))
-"""
+analytisk = []
+numerisk = []
 
-x = np.linspace(0,1,len(eigen_val1))
+N = int( len(tmp)/2 )
+for i in range(  N  ):
+    analytisk.append(tmp[i])
+    numerisk.append(tmp[N+i])
 
-plt.plot(x,eigen_val1,label="Egenvektor 1")
-#plt.plot(x,eigen_val2,label="Egenvektor 2")
-#plt.plot(x,eigen_val3,label="Egenvektor 3")
-#plt.plot(x,eigen_val4,label="Egenvektor 4")
-#plt.plot(x_val,num_val, label="Numerical value")
-plt.title("Utslag til bjelken",size=17)
-plt.ylabel("Utslag ; ",size=17)
-plt.xlabel("Posisjon ; ",size=17)
-plt.grid()
-plt.legend()
-plt.show()
-
+ana_ary = np.array(analytisk)
+num_ary= np.array(numerisk)
+print(abs((ana_ary-num_ary)/ana_ary))
